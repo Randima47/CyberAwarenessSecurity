a84dd9e4b131791e69da0643f848492629336413
Randima47 <ndivhorandima47@gmail.com>
2026-05-21 18:45:19 +0200

    add MemoryManager to store and recall user interests

---
+++ CyberAwarenessSecurity/MemoryManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CyberAwarenessSecurity
+{
+    public static class MemoryManager
+    {
+        // Dictionary to store user details or interests
+        private static Dictionary<string, string> memory = new Dictionary<string, string>();
+
+        // Save information
+        public static void Remember(string key, string value)
+        {
+            if (memory.ContainsKey(key))
+                memory[key] = value;
+            else
+                memory.Add(key, value);
+        }
+
+        // Retrieve information
+        public static string Recall(string key)
+        {
+            return memory.ContainsKey(key) ? memory[key] : null;
+        }
+
+        // Clear memory (optional, for reset)
+        public static void Clear()
+        {
+            memory.Clear();
+        }
+    }
+}
