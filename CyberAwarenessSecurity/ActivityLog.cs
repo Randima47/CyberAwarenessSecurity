215d07c46344dd5ed59a69b365eac33fc15ee7c8
Randima47 <ndivhorandima47@gmail.com>
2026-06-12 16:48:55 +0200

    Add ActivityLog class

---
+++ CyberAwarenessSecurity/ActivityLog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CyberAwarenessSecurity
+{
+    public class ActivityEntry
+    {
+        public DateTime Timestamp { get; set; }
+        public string Action { get; set; }
+    }
+
+    public static class ActivityLog
+    {
+        private static List<ActivityEntry> entries = new List<ActivityEntry>();
+
+        public static void Add(string action)
+        {
+            entries.Add(new ActivityEntry
+            {
+                Timestamp = DateTime.Now,
+                Action = action
+            });
+        }
+
+        public static List<ActivityEntry> GetEntries()
+        {
+            return new List<ActivityEntry>(entries);
+        }
+
+        public static void Clear()
+        {
+            entries.Clear();
+        }
+    }
+}
